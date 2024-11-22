2a790090b16ae32c30395affe8521c29629a9deb
matheusaguiarrr <matheustitanio5@gmail.com>
2024-11-22 14:00:51 -0300

    refactor: add new fields in the userFactiory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'cpf' => fake()->unique()->numerify('###.###.###-##'),
+            'phone' => fake()->phoneNumber(),
+            'user_type' => fake()->numberBetween(0, 3),
+            'image' => fake()->imageUrl()
         ];
     }
 
