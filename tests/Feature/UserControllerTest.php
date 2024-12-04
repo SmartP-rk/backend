7a06b8654e979a3efad6bef57ba22336c51f0dd9
matheusaguiarrr <matheustitanio5@gmail.com>
2024-12-04 16:41:09 -0300

    test: add validation test for name field length limit in UserController

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -6,15 +6,45 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_name_is_required(){
+        // Dados simulados para criar o usuário
+        $payload = [
+            'name' => '',
+            'email' => 'johndoe@example.com',
+            'password' => '!Password123',
+            'cpf' => '000.000.000-00',
+            'phone' => '(53) 99911-2233',
+            'user_type' => '1',
+        ];
+        // Chama a rota users do método store com os dados
+        $response = $this->postJson(route('users.store'), $payload);
+        // Verifica a estrutura e conteúdo da resposta
+        $response->assertStatus(422)->assertJsonValidationErrors(['name']);
+    }
+
+    public function test_name_must_not_excced_255_characters(){
+        // Dados simulados para criar o usuário
+        $payload = [
+            'name' => str_repeat('a', 256), // Cria uma string com 256 caracteres
+            'email' => 'johndoe@example.com',
+            'password' => '!Password123',
+            'cpf' => '000.000.000-00',
+            'phone' => '(53) 99911-2233',
+            'user_type' => '1',
+        ];
+        // Chama a rota users do método store com os dados
+        $response = $this->postJson(route('users.store'), $payload);
+        // Verifica se o status da resposta é 422 (Unprocessable Entity) e se a resposta contém o erro de validação para name
+        $response->assertStatus(422)->assertJsonValidationErrors(['name']);
+    }
+
     public function test_store_creates_user_and_returns_success_message()
     {
-        $this->withoutExceptionHandling();
         // Dados simulados para criar o usuário
         $payload = [
             'name' => 'John Doe',
