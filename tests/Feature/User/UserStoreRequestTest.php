bca09c7f2f628bf64f406839e78b98e7021a0459
matheusaguiarrr <matheustitanio5@gmail.com>
2024-12-05 17:15:12 -0300

    test: refactor user tests into separate files and adjust PHPUnit configuration

--- tests/Feature/User/UserStoreRequestTest.php
+++ tests/Feature/User/UserStoreRequestTest.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\User;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
-class UserControllerTest extends TestCase
+class UserStoreRequestTest extends TestCase
 {
     use RefreshDatabase;
-
     public function test_name_is_required(){
         // Dados simulados para criar o usuário
         $payload = [
@@ -75,7 +74,7 @@ class UserControllerTest extends TestCase
         $response->assertStatus(422)->assertJsonValidationErrors(['email']);
     }
 
-    public function teste_password_is_required(){
+    public function test_password_is_required(){
         // Dados simulados para criar o usuário
         $payload = [
             'name' => 'John Doe',
@@ -105,43 +104,5 @@ class UserControllerTest extends TestCase
         $response = $this->postJson(route('users.store'), $payload);
         // Verifica se o status da resposta é 422 (Unprocessable Entity) e se a resposta contém o erro de validação para password
         $response->assertStatus(422)->assertJsonValidationErrors(['password']);
-    }
-
-    public function test_store_creates_user_and_returns_success_message()
-    {
-        // Dados simulados para criar o usuário
-        $payload = [
-            'name' => 'John Doe',
-            'email' => 'johndoe@example.com',
-            'password' => '!Password123',
-            'cpf' => '000.000.000-00',
-            'phone' => '(53) 99911-2233',
-            'user_type' => '1',
-        ];
-
-        // Chama a rota do método store com os dados
-        $response = $this->postJson(route('users.store'), $payload);
-        // Verifica se o usuário foi criado no banco de dados
-        $this->assertDatabaseHas('users', [
-            'email' => 'johndoe@example.com'
-        ]);
-
-        // Verifica a estrutura e conteúdo da resposta
-        $response->assertStatus(201)
-                ->assertJsonStructure([
-                    'msg',
-                    'user' => ['id', 'name', 'email', 'created_at', 'updated_at']
-                ])
-                ->assertJson([
-                    'msg' => 'Usuário cadastrado com sucesso',
-                    'user' => [
-                        'name' => 'John Doe',
-                        'email' => 'johndoe@example.com'
-                    ]
-                ]);
-
-        // Valida que a senha foi armazenada como hash
-        $user = User::where('email', 'johndoe@example.com')->first();
-        $this->assertTrue(Hash::check('!Password123', $user->password));
     }
 }
